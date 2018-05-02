f164b4ebb3c3aa03c629f761d43791bd771d708e
Preston Fraser <pefbass@gmail.com>
2018-05-01 19:50:13 -0700

    building prepareColumns() + fixing some naming and variables

--- project2-1403225/codebase/rm/rm.h
+++ project2-1403225/codebase/rm/rm.h
@@ -80,7 +80,10 @@ private:
 
   void createTableDescriptor(vector<Attribute> &tableDescriptor);
   void createColumnDescriptor(vector<Attribute> &columnDescriptor);
-  void prepareTable(int table_id, const string &table_name, const string &file_name, void *buffer, int *tupleSize, vector<Attribute> &tableDescriptor);
+  void prepareTables(int table_id, const string &table_name, const string &file_name, void *buffer, vector<Attribute> &tableDescriptor);
+  void prepareColumns(int table_id, const string &column_name, int column_type, int column_length, int column_position, void *buffer, vector<Attribute> &tableDescriptor);
+
+  //Columns(table-id:int, column-name:varchar(50), column-type:int, column-length:int, column-position:int)
 
 
 };
