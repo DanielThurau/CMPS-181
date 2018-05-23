fd731c2a732071f79c7b41a622ded58028f67d2a
Morgan Grant <gramorgan@gmail.com>
2018-05-22 19:38:51 -0700

    fix test 2

--- project3-15/codebase/ix/ixtest_02.cc
+++ project3-15/codebase/ix/ixtest_02.cc
@@ -26,11 +26,6 @@ int testCase_2(const string &indexFileName, const Attribute &attribute)
     rid.pageNum = key;
     rid.slotNum = key+1;
 
-    RID rid2;
-    int key2 = 20;
-    rid2.pageNum = key2;
-    rid2.slotNum = key2 + 1;
-
     unsigned readPageCount = 0;
     unsigned writePageCount = 0;
     unsigned appendPageCount = 0;
@@ -57,8 +52,6 @@ int testCase_2(const string &indexFileName, const Attribute &attribute)
     // insert entry
     rc = indexManager->insertEntry(ixfileHandle, attribute, &key, rid);
     assert(rc == success && "indexManager::insertEntry() should not fail.");
-
-    indexManager->insertEntry(ixfileHandle, attribute, &key2, rid2);
 
     // collect counters
     rc = ixfileHandle.collectCounterValues(readPageCountAfter, writePageCountAfter, appendPageCountAfter);
