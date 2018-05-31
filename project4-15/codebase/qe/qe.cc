5f8c5b53058299c77c18069e347830ba498f0c5b
Preston Fraser <pefbass@gmail.com>
2018-05-31 16:01:35 -0700

    Building QE skeleton

--- project4-15/codebase/qe/qe.cc
+++ project4-15/codebase/qe/qe.cc
@@ -1,7 +1,58 @@
 
 #include "qe.h"
 
-Filter::Filter(Iterator* input, const Condition &condition) {
+Filter::Filter(Iterator* input, const Condition &condition)
+{
 }
 
-// ... the rest of your implementations go here
+Filter::~Filter()
+{
+}
+
+RC Filter::getNextTuple(void *data)
+{
+	return QE_EOF;
+}
+
+void Filter::getAttributes(vector<Attribute> &attrs) const
+{
+	return;
+}
+
+Project::Project(Iterator *input, const vector<string> &attrNames)
+{
+}
+
+Project::~Project()
+{
+}
+
+RC Project::getNextTuple(void *data)
+{
+	return QE_EOF;
+}
+
+void getAttributes(vector<Attribute> &attrs) const
+{
+	return;
+}
+
+INLJoin::INLJoin(Iterator *leftIn,
+	  IndexScan *rightIn,
+	  const Condition &condition)
+{
+}
+
+INLJoin::~INLJoin()
+{
+}
+
+RC INLJoin::getNextTuple(void *data)
+{
+	return QE_EOF;
+}
+
+void INLJoin::getAttributes(vector<Attribute> &attrs) const
+{
+	return;
+}
