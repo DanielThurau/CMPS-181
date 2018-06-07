a5612ea3f637c70dfa92c690317f4979626417a3
Morgan Grant <gramorgan@gmail.com>
2018-06-06 17:17:11 -0700

    fix issue with index creation

--- project4-15/codebase/rm/rm.cc
+++ project4-15/codebase/rm/rm.cc
@@ -1081,14 +1081,16 @@ RC RelationManager::createIndex(const string &tableName, const string &attribute
         return rc;
     
     // alloc a buffer big enough for the attribute + null indicator
-    void *key = malloc(attr->length + 1);
+    void *tuple = malloc(attr->length + 1);
 
     IXFileHandle ixfileHandle;
     im->openFile(index_filename, ixfileHandle);
-    while (scanner.getNextTuple(rid, key) != RM_EOF) {
+    while (scanner.getNextTuple(rid, tuple) != RM_EOF) {
         // insert each entry into the index
         // use key + 1 to skip null indicator
-        im->insertEntry(ixfileHandle, *attr, (char *) key + 1, rid);
+        void *key;
+        getAttrFromTuple(recordDescriptor, attr - recordDescriptor.begin(), tuple, key);
+        im->insertEntry(ixfileHandle, *attr, key, rid);
     }
     im->closeFile(ixfileHandle);
 
